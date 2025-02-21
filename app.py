db7eae2392fa5c9245ec74f22a8fe9280a9ed5d7
jdavromero <davidromeroa12@gmail.com>
2025-02-21 02:21:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import spacy
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+nlp = spacy.load("trained_ner_model")
 
-if __name__ == '__main__':
+@app.route("/", methods=["GET", "POST"])
+def index():
+    entities = []
+    if request.method == "POST":
+        text = request.form.get("text_input")
+        if text:
+            doc = nlp(text)
+            entities = [(ent.text, ent.label_) for ent in doc.ents]
+    return render_template("index.html", entities=entities)
+
+if __name__ == "__main__":
     app.run(debug=True)
